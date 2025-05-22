bf9713f08ea74f20dbe0db6bfe20aba18a50a7b0
ccadevida03 <c.cadevida03@gmail.com>
2025-05-22 10:01:08 +0800

    updated color

--- bars/properties.php
+++ bars/properties.php
@@ -1,7 +1,7 @@
 <?php
-$cards = "bg-success text-white text-lg";
+$cards = "bg-info text-white text-lg";
 $systemname = "Smile Save Dental Care";
-$sidebarColor = "bg-gradient-success";
+$sidebarColor = "bg-gradient-info";
 $superuser = "ssdc_admin2020";
 $dentist = [
     "Dr. Maria Regina I. Valencia",
