cf16d2c0bbd1e8819acd76a89a31e5ebaa6fc38d
ccadevida03 <c.cadevida03@gmail.com>
2025-05-30 16:19:26 +0800

    sending email

--- controllers/accessController.js
+++ controllers/accessController.js
@@ -4,23 +4,23 @@ function accessSystem() {
     //BUSINESS LOGIC - VALIDATION OF REQUEST / RESPONSE
     var username = document.getElementById("loginUser").value;
     var password = document.getElementById("loginPassword").value;
-    if (username != "" && password=="") {
+    if (username != "" && password == "") {
         document.getElementById("loginResult").innerHTML = "Please Enter your Password!";
-    } 
-    else if (username == "" && password!="") {
+    }
+    else if (username == "" && password != "") {
         document.getElementById("loginResult").innerHTML = "Please Enter your Username!";
-    } 
-    else if (username == null || username =="" && password== null || password=="") {
+    }
+    else if (username == null || username == "" && password == null || password == "") {
         document.getElementById("loginResult").innerHTML = "Fields are Empty";
     }
     else {
         document.getElementById("loginResult").innerHTML = "Checking You Account....";
-        validateLogin(username,password);
+        validateLogin(username, password);
     }
 
 }
 
-function validateLogin(username,password) {
+function validateLogin(username, password) {
     var fd = new FormData();
     fd.append('username', username);
     fd.append('password', password);
@@ -31,10 +31,11 @@ function validateLogin(username,password) {
         contentType: false,
         type: 'POST',
         success: function (result) {
-            result=result.trim();
+            result = result.trim();
+            console.log(result);
             if (result == "success") {
                 window.location.href = "basecode.php";
-            }else {
+            } else {
                 document.getElementById("loginResult").innerHTML = "Invalid Username or Password.";
             }
         }
