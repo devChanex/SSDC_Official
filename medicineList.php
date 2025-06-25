33538a153cdaaf8758fd345fc5903d4031ef0253
ccadevida03 <c.cadevida03@gmail.com>
2025-06-25 16:53:14 +0800

    updated medicine list controller version

--- medicineList.php
+++ medicineList.php
@@ -141,7 +141,7 @@
             <script src="js/sb-admin-2.min.js"></script>
             <script src="controllers/logOutConroller.js"></script>
             <script src="controllers/sessionController.js"></script>
-            <script src="controllers/getmedicineListController.js"></script>
+            <script src="controllers/getmedicineListController-v1.js"></script>
             <script src="js/custom-v2.js"></script>
 
 
