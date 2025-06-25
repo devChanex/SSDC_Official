3e1431561d8be652c59375b380fd8de20e110bc6
ccadevida03 <c.cadevida03@gmail.com>
2025-06-25 18:38:22 +0800

    updated consent and bleeding

--- index.php
+++ index.php
@@ -457,20 +457,14 @@
                                             </div>
                                         </div>
                                         <!-- 5 -->
-                                        <div class="row">
-                                            <div class="col-lg-12 mb-3">
-                                                <div class="d-flex align-items-center gap-2">
-                                                    <label for="bleedingTime" class="form-label mb-0 me-2"
-                                                        style="white-space: nowrap;">5. Bleeding Time:</label>
-                                                    <input class="form-control flex-grow-1" type="text"
-                                                        id="bleedingTime" placeholder="Specify bleeding time">
-                                                </div>
-                                            </div>
-                                        </div>
+
+                                        <input class="form-control flex-grow-1" type="hidden" id="bleedingTime"
+                                            placeholder="Specify bleeding time">
+
 
                                         <div class="row">
                                             <div class="col-lg-12 mb-2">
-                                                <label class="form-label fw-bold">6. For women only:</label>
+                                                <label class="form-label fw-bold">5. For women only:</label>
                                             </div>
 
                                             <!-- Are you pregnant? -->
