aa64a7e0a64ab609b2514b515ec5b0faf1eaaf20
Eysi27 <lirio.aaron@gmail.com>
2025-10-23 04:25:44 +0800

    Add fb page on prescription header

--- prescriptionList.php
+++ prescriptionList.php
@@ -127,7 +127,8 @@
 
                                     <p style="text-align: center; color: black; margin: 0;font-size:13px;">
 
-                                        <strong>Contact Number:</strong> 0919 009 3099 / (049) 539 0277
+                                        <strong>Contact Number:</strong> 0919 009 3099 / (049) 539 0277 <br>
+                                        <strong>Facebook Page:</strong> www.facebook.com/smilesavedental
                                     </p>
 
 
@@ -181,7 +182,7 @@
                                     </div>
 
 
-                                    -->
+                             
 
                                 </div>
                                 <div class="modal-footer d-print-none">
