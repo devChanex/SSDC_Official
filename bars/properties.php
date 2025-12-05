8d262880f3388cfe2ace89c0a55ff132f9056b59
Eysi27 <lirio.aaron@gmail.com>
2025-12-05 11:16:05 +0800

    Add Dr. Angelu AJ Del Rosario

--- bars/properties.php
+++ bars/properties.php
@@ -15,7 +15,8 @@ $dentist = [
     "Dr. Joyce Ann Gabayan",
     "Dr. Rolisa Lumba-Adove",
     "Dr. Marjorie Esguerra",
-    "Dr. Chicklet Buaya"
+    "Dr. Chicklet Buaya",
+    "Dr. Angelu AJ Del Rosario"
 
 ];
 $dentistSignature = "e-sign.png";
