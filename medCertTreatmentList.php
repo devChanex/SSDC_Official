365f59006ceb2553393da02087c8152979120a02
Eysi27 <lirio.aaron@gmail.com>
2026-02-18 10:30:50 +0800

    Fixed Medcert conifguration treatment list

--- medCertTreatmentList.php
+++ medCertTreatmentList.php
@@ -141,7 +141,7 @@
             <script src="js/sb-admin-2.min.js"></script>
             <script src="controllers/logOutConroller.js"></script>
             <script src="controllers/sessionController.js"></script>
-            <script src="controllers/getMedCertTreatmentListController.js"></script>
+            <script src="controllers/getMedCertTreatmentListController-v2.js"></script>
             <script src="js/custom-v2.js"></script>
 
 
