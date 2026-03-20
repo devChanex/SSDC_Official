9b54c21746232469073b5a9c3c5fcf00cece2ade
ccadevida03 <c.cadevida03@gmail.com>
2026-03-20 11:01:33 +0800

    added htaccess

--- services/getMEdHistoryService.php
+++ services/getMEdHistoryService.php
@@ -5,7 +5,7 @@ $result = $service->loadClientProfile();
 
 class ServiceClass
 {
-	
+
 	private $conn;
 	public function __construct()
 	{
@@ -20,19 +20,16 @@ class ServiceClass
 		return $stmt;
 	}
 	//DO NOT INCLUDE THIS CODE
-    public function loadClientProfile(){
-        $query = "select * from medhistory";
+	public function loadClientProfile()
+	{
+		$query = "select * from medhistory";
 		$stmt = $this->conn->prepare($query);
 		$stmt->execute();
 		if ($stmt->rowCount() > 0) {
-            echo'
-            <tr>
-                            <td>'.$row["clientid"].'</td>
-                            <td>'.$row["q1"].'</td>
-                            <td>'.$row["q2"].'</td>'
- 
- 
- 
-        }
-    }
+
+
+
+
+		}
+	}
 }
